e50d2a8a9e058af733bdb5422eb6ded10a2c938e
Mudasir Hussain <40722738+Mudasirrr@users.noreply.github.com>
2023-07-23 15:52:47 +1000

    Update Task1_4p.js

--- Task1_4p.js
+++ Task1_4p.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+var http = require('http');
+var path = require('path');
+
+var express = require('express');
+var router = express();
+var server = http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+
+console.log('Server is loading')
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server started");
+});
