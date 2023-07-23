2107be2ba62fd4d08b6b4e70a8369dc1dd514917
Mudasir Hussain <40722738+Mudasirrr@users.noreply.github.com>
2023-07-23 15:56:00 +1000

    Add files via upload

---
+++ client/js/client.js
@@ -0,0 +1,4 @@
+console.log('Client side.........');
+
+// Insert the 'Hey there!' text on the template page
+document.getElementById('greeting').innerText = 'Hey there! its my first task in Deakin University';
